ff968bbe0154773c31f0a37a3db453c6735a330c
Dimitri <dimitri.reg@gmail.com>
2016-02-16 22:13:55 +0100

    Increased network polling delay to 10 seconds

--- spring-boot-jpa-docker-bluemix-webapp/src/main/docker/init.sh
+++ spring-boot-jpa-docker-bluemix-webapp/src/main/docker/init.sh
@@ -2,7 +2,7 @@
 
 while ! exec 6<>/dev/tcp/${MYSQL_PORT_3306_TCP_ADDR}/${MYSQL_PORT_3306_TCP_PORT}; do
     echo "Trying to connect to MySQL at ${MYSQL_PORT}..."
-    sleep 1
+    sleep 10
 done
 
 java -Djava.security.egd=file:/dev/./urandom -Dspring.profiles.active=container -Xms128m -Xmx256m -jar /app.jar
