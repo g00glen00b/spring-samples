0df1852502fd128d727bf815ec8bda0d46079568
g00glen00b <me@g00glen00b.be>
2016-06-05 22:22:51 +0200

    Implemented DAO

---
+++ spring-boot-swagger/src/main/java/be/g00glen00b/dto/TaskDTO.java
@@ -0,0 +1,40 @@
+package be.g00glen00b.dto;
+
+import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Size;
+
+public class TaskDTO {
+    private Long id;
+    @NotNull(message = "NotNull.taskDTO.description")
+    @Size(min = 1, max = 64, message = "Size.taskDTO.description")
+    private String description;
+    private boolean completed;
+
+    public TaskDTO() {
+    }
+
+    public TaskDTO(Long id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
---
+++ spring-boot-swagger/src/main/java/be/g00glen00b/entity/Task.java
@@ -0,0 +1,49 @@
+package be.g00glen00b.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "task")
+public class Task {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+    @Column(name = "description")
+    private String description;
+    @Column(name = "completed")
+    private boolean completed;
+
+    public Task(Long id, String description, boolean completed) {
+        this.id = id;
+        this.description = description;
+        this.completed = completed;
+    }
+
+    public Task() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public boolean isCompleted() {
+        return completed;
+    }
+    public void setCompleted(boolean completed) {
+        this.completed = completed;
+    }
+}
+
