a786a1e1ea558ed62eeefddd15b9037445e1667b
g00glen00b <me@g00glen00b.be>
2016-06-05 22:22:37 +0200

    Adding dependencies + Fixing indentation + Using YAML + Removing default tests

--- spring-boot-swagger/src/main/java/be/g00glen00b/SpringBootSwaggerApplication.java
+++ spring-boot-swagger/src/main/java/be/g00glen00b/SpringBootSwaggerApplication.java
@@ -6,7 +6,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 @SpringBootApplication
 public class SpringBootSwaggerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SpringBootSwaggerApplication.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootSwaggerApplication.class, args);
+    }
 }
