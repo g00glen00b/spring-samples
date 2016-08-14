c4def56993a4f429698681553b0cad8f2db0b453
g00glen00b <me@g00glen00b.be>
2016-08-14 20:12:11 +0200

    Initial implementation application consuming microservice

---
+++ spring-boot-eureka/spring-boot-eureka-consumer/src/main/java/be/g00glen00b/SpringBootEurekaConsumerApplication.java
@@ -0,0 +1,12 @@
+package be.g00glen00b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootEurekaConsumerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootEurekaConsumerApplication.class, args);
+	}
+}
