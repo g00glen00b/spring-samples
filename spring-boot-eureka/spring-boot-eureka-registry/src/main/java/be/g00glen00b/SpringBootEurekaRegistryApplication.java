662847e09245d0dc073a5b7b4254b75ccbf544c8
g00glen00b <me@g00glen00b.be>
2016-08-14 20:02:03 +0200

    Implemented Eureka registry example

---
+++ spring-boot-eureka/spring-boot-eureka-registry/src/main/java/be/g00glen00b/SpringBootEurekaRegistryApplication.java
@@ -0,0 +1,14 @@
+package be.g00glen00b;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+
+@SpringBootApplication
+@EnableEurekaServer
+public class SpringBootEurekaRegistryApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootEurekaRegistryApplication.class, args);
+    }
+}
