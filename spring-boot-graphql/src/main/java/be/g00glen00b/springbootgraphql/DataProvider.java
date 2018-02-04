fcad8dbc09f04dcf08c6aa7537b21b2e827a5da5
g00glen00b <me@g00glen00b.be>
2018-02-04 16:22:41 +0100

    Added CORS header

--- spring-boot-graphql/src/main/java/be/g00glen00b/springbootgraphql/DataProvider.java
+++ spring-boot-graphql/src/main/java/be/g00glen00b/springbootgraphql/DataProvider.java
@@ -24,7 +24,7 @@ public class DataProvider implements CommandLineRunner {
     public void run(String... strings) {
         Profile author = profileRepository.save(new Profile(null, "g00glen00b", "The author of this blog"));
         Profile admin = profileRepository.save(new Profile(null, "admin", "The administrator of this blog"));
-        Article article1 = articleRepository.save(new Article(null, "Hello wold", "This is a hello world", author.getId()));
+        Article article1 = articleRepository.save(new Article(null, "Hello world", "This is a hello world", author.getId()));
         Article article2 = articleRepository.save(new Article(null, "Foo", "Bar", admin.getId()));
         commentRepository.save(new Comment(null, "Do you like this article?", article1.getId(), author.getId()));
         commentRepository.save(new Comment(null, "This is a great article", article1.getId(), admin.getId()));
--- spring-boot-graphql/src/main/java/be/g00glen00b/springbootgraphql/SpringBootGraphqlApplication.java
+++ spring-boot-graphql/src/main/java/be/g00glen00b/springbootgraphql/SpringBootGraphqlApplication.java
@@ -1,7 +1,15 @@
 package be.g00glen00b.springbootgraphql;
 
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.stream.Collectors;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.web.cors.CorsConfiguration;
+import org.springframework.web.cors.UrlBasedCorsConfigurationSource;
+import org.springframework.web.filter.CorsFilter;
 
 @SpringBootApplication
 public class SpringBootGraphqlApplication {
@@ -9,4 +17,16 @@ public class SpringBootGraphqlApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(SpringBootGraphqlApplication.class, args);
 	}
+
+	@Bean
+	public CorsFilter corsFilter() {
+        final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
+        final CorsConfiguration config = new CorsConfiguration();
+        config.setAllowCredentials(true);
+        config.setAllowedOrigins(Collections.singletonList("*"));
+        config.setAllowedHeaders(Collections.singletonList("*"));
+        config.setAllowedMethods(Arrays.stream(HttpMethod.values()).map(HttpMethod::name).collect(Collectors.toList()));
+        source.registerCorsConfiguration("/**", config);
+        return new CorsFilter(source);
+    }
 }
