f9c2736e0e4476fe03903f1f1671c5f676ac8dd5
Ramazan Sakin <ramazansakin63@gmail.com>
2020-10-23 14:23:47 +0300

    eureka-consumer - lombok integration

--- spring-boot-eureka/spring-boot-eureka-consumer/src/main/java/be/g00glen00b/web/TaskController.java
+++ spring-boot-eureka/spring-boot-eureka-consumer/src/main/java/be/g00glen00b/web/TaskController.java
@@ -1,17 +1,16 @@
 package be.g00glen00b.web;
 
 import be.g00glen00b.service.TaskServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
+import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
-@Controller
-@RequestMapping("/")
+@RestController
+@RequiredArgsConstructor
 public class TaskController {
-    @Autowired
-    private TaskServiceImpl service;
+
+    private final TaskServiceImpl service;
 
     @GetMapping()
     public ModelAndView findAll() {
