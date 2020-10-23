66028c3c6e19cdd47dc2c5271fd6e7ec59cd7001
Ramazan Sakin <ramazansakin63@gmail.com>
2020-10-23 12:02:30 +0300

    sonarLint issues fixed

--- spring-boot-angularjs/src/main/java/be/g00glen00b/web/TaskController.java
+++ spring-boot-angularjs/src/main/java/be/g00glen00b/web/TaskController.java
@@ -1,8 +1,5 @@
 package be.g00glen00b.web;
 
-import java.util.List;
-import java.util.Locale;
-import javax.validation.Valid;
 import be.g00glen00b.service.TaskNotFoundException;
 import be.g00glen00b.service.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,13 +7,11 @@ import org.springframework.context.MessageSource;
 import org.springframework.context.i18n.LocaleContextHolder;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.MethodArgumentNotValidException;
-import org.springframework.web.bind.annotation.ExceptionHandler;
-import org.springframework.web.bind.annotation.PathVariable;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
-import org.springframework.web.bind.annotation.ResponseStatus;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
+
+import javax.validation.Valid;
+import java.util.List;
+import java.util.Locale;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -26,22 +21,22 @@ public class TaskController {
     @Autowired
     private MessageSource messageSource;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
     public List<TaskDTO> findAll() {
         return service.findAll();
     }
 
-    @RequestMapping(method = RequestMethod.POST)
+    @PostMapping()
     public TaskDTO save(@Valid @RequestBody TaskDTO input) {
         return service.save(input);
     }
 
-    @RequestMapping(method = RequestMethod.PUT, value = "/{id}")
+    @PutMapping(value = "/{id}")
     public TaskDTO update(@PathVariable Long id, @Valid @RequestBody TaskDTO input) {
         return service.update(id, input);
     }
 
-    @RequestMapping(method = RequestMethod.DELETE, value = "/{id}")
+    @DeleteMapping(value = "/{id}")
     public void delete(@PathVariable Long id) {
         service.delete(id);
     }
--- spring-boot-eureka/spring-boot-eureka-service/src/main/java/be/g00glen00b/web/TaskController.java
+++ spring-boot-eureka/spring-boot-eureka-service/src/main/java/be/g00glen00b/web/TaskController.java
@@ -1,12 +1,13 @@
 package be.g00glen00b.web;
 
-import java.util.List;
 import be.g00glen00b.dto.TaskDTO;
 import be.g00glen00b.service.TaskServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/tasks")
@@ -14,7 +15,7 @@ public class TaskController {
     @Autowired
     private TaskServiceImpl service;
 
-    @RequestMapping(method = RequestMethod.GET)
+    @GetMapping()
     public List<TaskDTO> findAll() {
         return service.findAll();
     }
